c31138ca8c6464c07519a69d6dee492c9b8eaeec
Axel <axelmontes92@gmail.com>
2017-04-01 12:09:10 -0600

    Acondicionamiento de código

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    adaptiveHeight: true,
+    mode: 'fade',
+    auto: true,
+  });
+
+  $('.slider2').bxSlider({
+   adaptiveHeight: true,
+   mode: 'vertical',
+   auto: true,
+ });
+
+  $('.slider3').bxSlider({
+    adaptiveHeight: true,
+    mode: 'vertical',
+    auto: true,
+  });
+
+  $('.nav a').click(function(){
+    $('.navbar-ex1-collapse').collapse('hide');
+  });
+
+  /*Contador*/
+  var target_date = new Date('Jun, 31, 2017').getTime();
+  // variables for time units
+  var days, hours, minutes, seconds;
+  // get tag element
+  var countdown = document.getElementById('countdown');
+  // update the tag with id "countdown" every 1 second
+  setInterval(function () {
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+
+    // format countdown string + set tag value
+    countdown.innerHTML = '<span class="days">' + days +  ' <b>dias</b></span> <span class="hours">' + hours + ' <b>horas</b></span> <span class="minutes">' + minutes + ' <b>minutos</b></span> <span class="seconds">' + seconds + ' <b>segundos</b></span>';  
+  }, 1000);
+});
